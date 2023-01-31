130592350289b2479ad8c22db0dde59d6edc55e5
Anton <belix888@yandex.ru>
2023-01-31 21:34:59 +0500

    OОП Введение - Задача 1

--- src/OopStart/Automobile.java
+++ src/OopStart/Automobile.java
@@ -1,6 +1,24 @@
 package OopStart;
 
 public class Automobile {
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int productionYear;
+    public String productionCountry;
 
-    
+    @Override
+    public String toString() {
+        return "Automobile{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", productionYear=" + productionYear +
+                ", productionCountry='" + productionCountry + '\'' +
+                '}';
+    }
+
+
 }

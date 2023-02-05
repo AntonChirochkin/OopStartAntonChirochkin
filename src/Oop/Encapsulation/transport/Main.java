eefbf1e7afb714fdfe6bff2b353fdabc387a660c
Anton <belix888@yandex.ru>
2023-02-05 16:35:13 +0500

    ООП Инкапсуляция: Домашка

--- src/Oop/Encapsulation/transport/Main.java
+++ src/Oop/Encapsulation/transport/Main.java
@@ -1,7 +1,25 @@
 package Oop.Encapsulation.transport;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Мир");
+        Car car = new Car("BMW",
+                "X7",
+                -1.0,
+                " ",
+                2040,
+                " ",
+                " ",
+                " ",
+                "A745PO196",
+                0, true);
+
+
+        System.out.println(car);
+
+        car.winterSummer(LocalDate.now().getMonthValue());
+
+        Car.Key key = new Car.Key(true, false);
     }
 }
--- src/Oop/Encapsulation/transport/Validate.java
+++ src/Oop/Encapsulation/transport/Validate.java
@@ -1,5 +1,10 @@
 package Oop.Encapsulation.transport;
 
 public class Validate {
-    String model;
+    public static String validateString(String value, String defaultValue) {
+        return (value == null || value.isBlank() || value.isEmpty()) ? defaultValue : value;
+    }
+    public static Integer validateNum (Integer value) {
+        return value == null || value < 0 ? 0 : value;
+    }
 }

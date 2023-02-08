75432c41b7469ca107d25c93a42c2c7597ef1bae
Anton <belix888@yandex.ru>
2023-02-08 21:59:19 +0500

    ООП. Наследование. Домашка - решение.

--- src/Oop/Nasledovanie/Main.java
+++ src/Oop/Nasledovanie/Main.java
@@ -1,54 +1,6 @@
 package Oop.Nasledovanie;
 
 public class Main {
-    public static void main(String[] args) {
-        Car car1 = new Car("Audi",
-                "Q8",
-                2023,
-                "Germany",
-                "Black",
-                300,
-                12,
-                "Automat",
-                "",
-                "A777CC196",
-                5);
 
-        Car car2 = new Car("BMV",
-                "X7",
-                2023,
-                "Germany",
-                "white",
-                270,
-                6.0,
-                "Automat",
-                "",
-                "A888CC196",
-                5);
-
-        Car car3 = new Car("Mersedes",
-                "GLS",
-                2023,
-                "Germany",
-                "red",
-                250,
-                8.0,
-                "Automat",
-                "",
-                "A555CC196",
-                7);
-
-        System.out.println(car1);
-        System.out.println(car2);
-        System.out.println(car3);
-
-        Bus bus1 = new Bus("Лиаз", "667", 1974, "Советский союз", "Желтый", 105);
-        Bus bus2 = new Bus("ЛАЗ", "695", 1971, "Советский союз", "", 115);
-        Bus bus3 = new Bus("Икарус", "250 SL", 1962, "Венгрия", "Красный", 105);
-
-        System.out.println(bus1);
-        System.out.println(bus2);
-        System.out.println(bus3);
-    }
 }
 
--- src/Oop/Nasledovanie/Transport.java
+++ src/Oop/Nasledovanie/Transport.java
@@ -1,57 +1,5 @@
 package Oop.Nasledovanie;
 
 public class Transport {
-    private final String brand;
-    private final String model;
-    private final int productionYear;
-    private final String productionCountry;
-    private String bodyWorkColor;
-    private double maxSpeed;
 
-    public Transport(String brand, String model, int productionYear, String productionCountry, String bodyWorkColor, double maxSpeed) {
-        this.brand = brand;
-        this.model = model;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
-        this.bodyWorkColor = validateString(bodyWorkColor);
-        this.maxSpeed = validateMaxSpeed(maxSpeed);
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getProductionYear() {
-        return productionYear;
-    }
-
-    public String getProductionCountry() {
-        return productionCountry;
-    }
-
-    public String getBodyWorkColor() {
-        return bodyWorkColor;
-    }
-
-    public double getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public void setBodyWorkColor(String bodyWorkColor) {
-        this.bodyWorkColor = bodyWorkColor;
-    }
-
-    public void setMaxSpeed(int maxSpeed) {
-        this.maxSpeed = maxSpeed;
-    }
-    public static String validateString(String value) {
-        return (value == null || value.isBlank() || value.isEmpty()) ? "default" : value;
-    }
-    public static double validateMaxSpeed(double value) {
-        return value < 0 ? 10 : value;
-    }
 }
